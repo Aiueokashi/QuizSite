950b1a10832e80f2956d0ca8024e5ac293151f41
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2022-06-27 12:00:39 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,14 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="10" md="8" lg="6" xl="5">
-      <ChemistryTableComponent />
+      <h1>クイズは左のメニューから<h1>
     </v-col>
   </v-row>
 </template>
 
 <script>
-import ChemistryTableComponent from '../components/ChemistryTable'
 export default {
   name: 'IndexPage',
 
-  components: {
-    ChemistryTableComponent,
-  },
 }
 </script>
