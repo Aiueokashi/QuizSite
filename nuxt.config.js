00e300cf844dc3b02e09a3c3f408929b1f317169
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2022-06-27 11:04:07 +0900

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,9 +5,9 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s',
-    title: 'english-quiz-nuxt',
+    title: 'クイズ用サイト',
     htmlAttrs: {
-      lang: 'en',
+      lang: 'ja',
     },
     meta: [
       { charset: 'utf-8' },
