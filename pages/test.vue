56e4c0afb1ec7d8a042dcecf06779075935a5e4f
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2022-06-27 15:51:50 +0000

    benzen

--- pages/test.vue
+++ pages/test.vue
@@ -1,8 +1,8 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="10" md="8" lg="6" xl="5">
-      <link id="Cuban" href="../chemData/2D/cuban.mol" type="chemical/x-mdl-molfile" />
-      <div id="composer" style="width:600px;height:400px" data-widget="Kekule.Editor.Composer" data-chem-obj="url(#Cuban)"></div>
+      <link id="Benzen" href="chemData/2D/benzene.mol" type="chemical/x-mdl-molfile" />
+      <div id="composer" style="width:600px;height:400px" data-widget="Kekule.Editor.Composer" data-chem-obj="url(#Benzen)"></div>
     </v-col>
   </v-row>
 </template>
