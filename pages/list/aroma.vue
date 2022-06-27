09726da620d91ef27ce7be88e5d7eda5ebfabfe2
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2022-06-27 11:58:41 +0900

    Update and rename _id.vue to aroma.vue

---
+++ pages/list/aroma.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="10" md="8" lg="6" xl="5">
+      <ChemistryTableComponent />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import ChemistryTableComponent from '../components/ChemistryTable'
+export default {
+  components: {
+    ChemistryTableComponent,
+  },
+}
+</script>
