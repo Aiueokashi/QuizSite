add31e35dd401c38302610aaa05491799252529d
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2022-06-28 08:21:26 +0900

    Update test.vue

--- pages/test.vue
+++ pages/test.vue
@@ -1,7 +1,10 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="10" md="8" lg="6" xl="5">
-      <div id="composer" style="width:600px;height:400px" data-widget="Kekule.Editor.Composer" data-chem-obj="url(#Benzene)"></div>
+      <div id="chemViewer" style="width:100px;height:60px"
+		  data-widget="Kekule.ChemWidget.Viewer2D" data-enable-toolbar="true" data-auto-size="false" data-padding="20"
+		  data-toolbar-evoke-modes="[0]"
+		  data-chem-obj="url(#)"></div>
     </v-col>
   </v-row>
 </template>
@@ -11,9 +14,27 @@
 
 export default {
   name: "TestPage",
+  head () {
+    return {
+      link: [
+        { id: 'サリチル酸', type: 'chemical/x-mdl-molfile', href: 'chemData/2D/サリチル酸.mol'}
+      ]
+    }
+  }
   mounted() {
-    
-  },
+    var resId = 'url(#サリチル酸)';
+      Kekule.IO.loadResourceData(resId, function(mol, success)
+      {
+        if (success)
+        {
+          console.log('Loading from ' + resId + ' Successful');
+        }
+        else
+        {
+          console.log('Loading from ' + resId + ' Failed');
+        }
+      });
+    },
 
-};
+  };
 </script>
