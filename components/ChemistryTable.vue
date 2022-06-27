051f0714aa9925d1b4e52835a34b021a1f0ddca3
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2022-06-27 11:21:47 +0900

    Create ChemistryTable.vue

---
+++ components/ChemistryTable.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-card>
+    <v-card-title>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+      <v-data-table
+        :headers="headers"
+        :items="items"
+        :search="search"
+        :items-per-page="10"
+        :footer-props="footerProps"
+        class="elevation-1"
+      ></v-data-table>
+    </v-card-title>
+  </v-card>
+</template>
+
+<script>
+import aromaticCompounds from '@/static/chemistry/aromatic_compounds.json'
+export default {
+  data() {
+    return {
+      search: '',
+      footerProps: {
+        'items-per-page-options': [10, 25, 50, 75, { text: 'All', value: -1 }],
+      },
+      headers: [
+        {
+          text: '化合物名',
+          align: 'start',
+          value: 'name',
+        },
+        { text: '化学式', value: 'formula' },
+        { text: '別名', value: 'alias' },
+        { text: '画像', value: 'img' },
+      ],
+      items: aromaticCompounds,
+    }
+  },
+}
+</script>
