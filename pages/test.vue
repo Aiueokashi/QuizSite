d34e56549d5ff6c56e4abf6837f60232596fb3cd
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2022-06-28 10:04:42 +0900

    Update test.vue

--- pages/test.vue
+++ pages/test.vue
@@ -1,8 +1,8 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="10" md="8" lg="6" xl="5">
-    <link id="benzene" type="chemical/x-mdl-molfile" href="../static/chemData/benzene.mol" />
-    <link id="サリチル酸" type="chemical/x-mdl-molfile" href="../static/chemData/2D/サリチル酸.mol" />
+    <link id="benzene" type="chemical/x-mdl-molfile" href="static/chemData/benzene.mol" />
+    <link id="サリチル酸" type="chemical/x-mdl-molfile" href="static/chemData/2D/サリチル酸.mol" />
     <span id="viewer2D1" style="display:block" data-widget="Kekule.ChemWidget.Viewer2D"
       data-chem-obj="url(#benzene)" data-predefined-setting="basic" data-auto-size="true"></span>
      <span id="viewer2D1" style="display:block" data-widget="Kekule.ChemWidget.Viewer2D"
@@ -16,7 +16,6 @@
 
 export default {
   name: "TestPage",
-  /*
   head() {
       return {
         link: [
@@ -33,7 +32,7 @@ export default {
           },
         ]
       }
-    },*/
+    },
   mounted() {
   
     },
