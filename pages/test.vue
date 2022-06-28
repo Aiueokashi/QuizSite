46430da56f2ca5da3d1cb9e3931ec43fb6ba0129
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2022-06-28 10:01:31 +0900

    Update test.vue

--- pages/test.vue
+++ pages/test.vue
@@ -1,8 +1,8 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="10" md="8" lg="6" xl="5">
-    <link id="benzene" type="chemical/x-cml" href="../static/chemData/benzene.mol" />
-    <link id="サリチル酸" type="chemical/x-cml" href="../static/chemData/2D/サリチル酸.mol" />
+    <link id="benzene" type="chemical/x-mdl-molfile" href="../static/chemData/benzene.mol" />
+    <link id="サリチル酸" type="chemical/x-mdl-molfile" href="../static/chemData/2D/サリチル酸.mol" />
     <span id="viewer2D1" style="display:block" data-widget="Kekule.ChemWidget.Viewer2D"
       data-chem-obj="url(#benzene)" data-predefined-setting="basic" data-auto-size="true"></span>
      <span id="viewer2D1" style="display:block" data-widget="Kekule.ChemWidget.Viewer2D"
@@ -16,6 +16,24 @@
 
 export default {
   name: "TestPage",
+  /*
+  head() {
+      return {
+        link: [
+          {
+            hid: 'サリチル酸',
+            id: 'サリチル酸',
+            type: 'chemical/x-mdl-molfile',
+            href: '../static/chemData/2D/サリチル酸.mol'
+          },{
+            hid: 'benzene',
+            id: 'benzene',
+            type: 'chemical/x-mdl-molfile',
+            href: '../static/chemData/benzene.mol'
+          },
+        ]
+      }
+    },*/
   mounted() {
   
     },
