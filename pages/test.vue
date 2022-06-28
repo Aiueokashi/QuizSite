425fc2602e85ec3fa5499f38c940c16088558c64
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2022-06-28 10:55:57 +0900

    Update test.vue

--- pages/test.vue
+++ pages/test.vue
@@ -1,9 +1,8 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="10" md="8" lg="6" xl="5">
-    <img src="/v.png" />
     <script id="サリチル酸" type="chemical/x-mdl-molfile" src="/chemData/2D/サリチル酸.mol"></script>
-     <span id="viewer2D1" style="display:block" data-widget="Kekule.ChemWidget.Viewer2D"
+     <span max-height="300px" max-width="300px" id="viewer2D1" style="display:block" data-widget="Kekule.ChemWidget.Viewer2D"
       data-chem-obj="url(#サリチル酸)" data-predefined-setting="basic" data-auto-size="true"></span>
     </v-col>
   </v-row>
