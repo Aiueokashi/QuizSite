896c98967c1273604288bbe1c1980d370affba93
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2022-06-28 09:02:47 +0900

    Update test.vue

--- pages/test.vue
+++ pages/test.vue
@@ -2,9 +2,9 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="10" md="8" lg="6" xl="5">
     <span id="viewer2D1" style="display:block" data-widget="Kekule.ChemWidget.Viewer2D"
-      data-chem-obj="url(../chemData/benzene.mol)" data-predefined-setting="basic" data-auto-size="true"></span>
+      data-chem-obj="url(chemData/benzene.mol)" data-predefined-setting="basic" data-auto-size="true"></span>
      <span id="viewer2D1" style="display:block" data-widget="Kekule.ChemWidget.Viewer2D"
-      data-chem-obj="url(../chemData/2D/サリチル酸.mol)" data-predefined-setting="basic" data-auto-size="true"></span>
+      data-chem-obj="url(chemData/2D/サリチル酸.mol)" data-predefined-setting="basic" data-auto-size="true"></span>
     </v-col>
   </v-row>
 </template>
