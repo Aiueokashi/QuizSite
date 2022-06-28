d176c04eca7ca3f14a0469904a494f1b224848b8
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2022-06-28 09:54:55 +0900

    Update test.vue

--- pages/test.vue
+++ pages/test.vue
@@ -1,10 +1,12 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="10" md="8" lg="6" xl="5">
+    <link id="benzene" type="chemical/x-cml" href="../static/chemData/benzene.mol" />
+    <link id="サリチル酸" type="chemical/x-cml" href="../static/chemData/2D/サリチル酸.mol" />
     <span id="viewer2D1" style="display:block" data-widget="Kekule.ChemWidget.Viewer2D"
-      data-chem-obj="../static/chemData/benzene.mol" data-predefined-setting="basic" data-auto-size="true"></span>
+      data-chem-obj="url(#benzene)" data-predefined-setting="basic" data-auto-size="true"></span>
      <span id="viewer2D1" style="display:block" data-widget="Kekule.ChemWidget.Viewer2D"
-      data-chem-obj="../static/chemData/2D/サリチル酸.mol" data-predefined-setting="basic" data-auto-size="true"></span>
+      data-chem-obj="url(#サリチル酸)" data-predefined-setting="basic" data-auto-size="true"></span>
     </v-col>
   </v-row>
 </template>
