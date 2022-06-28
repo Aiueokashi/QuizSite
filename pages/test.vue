3fffdf1c4d744d6c52d7d99a061ee621ff658415
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2022-06-28 10:21:02 +0900

    Update test.vue

--- pages/test.vue
+++ pages/test.vue
@@ -1,8 +1,7 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="10" md="8" lg="6" xl="5">
-    <link id="benzene" type="chemical/x-mdl-molfile" href="static/chemData/2D/benzenoanthracene.mol" />
-    <link id="サリチル酸" type="chemical/x-mdl-molfile" href="static/chemData/2D/サリチル酸.mol" />
+    <script id="chem2" type="chemical/x-mdl-molfile" src="static/chemData/2D/benzenoanthracene.mol"></script>
     <script id="chem1" type="chemical/x-mdl-molfile">
 Marvin  06041312482D          
 
@@ -32,7 +31,7 @@ M  END
     <span id="viewer2D1" style="display:block" data-widget="Kekule.ChemWidget.Viewer2D"
       data-chem-obj="url(#chem1)" data-predefined-setting="basic" data-auto-size="true"></span>
      <span id="viewer2D1" style="display:block" data-widget="Kekule.ChemWidget.Viewer2D"
-      data-chem-obj="url(#サリチル酸)" data-predefined-setting="basic" data-auto-size="true"></span>
+      data-chem-obj="url(#chem2)" data-predefined-setting="basic" data-auto-size="true"></span>
     </v-col>
   </v-row>
 </template>
